2dfd9d7f85ce80f87551a97ab9daeb1b2392c27c
MRSS <mateusyasd@hotmail.com>
2022-01-25 17:22:58 -0300

    changed characters to plus and minus

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -10,3 +10,9 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	velocity.y = min(velocity.y, maxspeed.y)
 	velocity = move_and_slide(velocity, UP)
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+		-0.6 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0 
+	)
