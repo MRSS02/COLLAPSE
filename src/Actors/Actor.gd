41773b01e6a8da30f7a866d5266a22ef4ee4c155
MRSS <mateusyasd@hotmail.com>
2022-01-25 15:22:42 -0300

    added (weird) jump

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -8,4 +8,4 @@ var velocity: = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	velocity.y = min(velocity.y, maxspeed.y)
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity)
--- src/Actors/Player0.gd
+++ src/Actors/Player0.gd
@@ -1,1 +1,11 @@
 extends Actor
+
+func _physics_process(delta: float) -> void:
+	var direction: = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+		-1.0 if Input.is_action_just_pressed("jump") else 0.0 
+	)
+	var orientation = 1 
+	velocity.x = (maxspeed.x) * direction.x * orientation 	
+	velocity.y += (maxspeed.y) * direction.y
+	print(direction.x) 
