42fbc5bd53ff6fffb6a65a6a1602ca4a55ec24d7
MRSS <mateusyasd@hotmail.com>
2022-01-24 23:04:50 -0300

    added a level

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 class_name Actor
 
+export var maxspeed: = Vector2(300.0, 500.0)
 export var gravity: = 300.0
 var velocity: = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
+	velocity.y = min(velocity.y, maxspeed.y)
 	move_and_slide(velocity)
