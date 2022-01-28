6d029d705d74a55abe95e4d4ebe4c5c71ccad220
MRSS <mateusyasd@hotmail.com>
2022-01-28 12:06:34 -0300

    idk

--- src/Items/minus_item.gd
+++ src/Items/minus_item.gd
@@ -5,4 +5,6 @@ var minus = preload("res://assets/sprites/minus.png")
 func _on_minus_body_entered(body: Node) -> void:
 	if body.has_method("set_orientation"):
 		body.set_orientation(-1)
+		body.set_collision_mask_bit(4, false)
+		body.set_collision_mask_bit(5, true)
 		body.get_node("sprite").set_texture(minus)
--- src/Items/plus_item.gd
+++ src/Items/plus_item.gd
@@ -5,4 +5,6 @@ var plus = preload("res://assets/sprites/plus.png")
 func _on_plus_item_body_entered(body: Node) -> void:
 	if body.has_method("set_orientation"):
 		body.set_orientation(1)
+		body.set_collision_mask_bit(4, true)
+		body.set_collision_mask_bit(5, false)
 		body.get_node("sprite").set_texture(plus)
